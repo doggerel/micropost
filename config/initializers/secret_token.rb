0e2e64af68c6946c4d37d7902891b6303dbe809a
grit <gritty@sympatico.ca>
2012-01-17 11:59:54 -0500

    Initial comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Micropost::Application.config.secret_token = 'dbb2ea0ec75f4e1ad61e6eb7d895e3992810ae6f1960762d6d262931143ec2173f3129af3cadbef82b10727ac8fd29b57dfc10e4b0fef6f0f0a461c7322bf222'
