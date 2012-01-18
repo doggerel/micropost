1eb1336510fdb94c8ca2f5d70ea11295e47e03fe
grit <gritty@sympatico.ca>
2012-01-18 11:50:00 -0500

    added some layout link tests

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
-  it "should have a home page '/'" do
+  it "should have a home page  at '/'" do
     get '/'
     response.should have_selector('title', :content =>"Home")
+  end
+  it "should have a contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title',
+                                  :content =>"Contact"
+    )
+  end
+  it "should have a help page at '/help'" do
+    get '/help'
+    response.should have_selector('title',
+                                 :content =>help)
   end
 end
